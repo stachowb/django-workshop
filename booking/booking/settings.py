a4f584fc9bcf84f5d405b6cd86ae20f4e49a3998
stachowb <stachow.bartlomiej@gmail.com>
2021-04-16 09:54:11 +0200

    added edit view

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g@)uf-ub5bowl+v-+90qrimh12^yn0r0sfo@-_b4@@h&$3s5#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["workshop-booking.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
