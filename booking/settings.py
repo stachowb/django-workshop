e525ea4921bbf819a0fc5ed5aa036d507b5d6ee4
stachowb <stachow.bartlomiej@gmail.com>
2021-04-16 19:32:17 +0200

    changed settings

--- booking/settings.py
+++ booking/settings.py
@@ -136,5 +136,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-# import django_on_heroku
-# django_on_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
