2696a7098674f1b48a042950c0cef9f5a28165ea
stachowb <stachow.bartlomiej@gmail.com>
2021-04-16 09:52:01 +0200

    added edit view

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'NAME': 'booking',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'USER': 'postgres',
-        'PASSWORD': 'bartek93',
+        'PASSWORD': 'coderslab',
         'HOST': 'localhost',
     }
 }
@@ -126,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    BASE_DIR / "static"
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
