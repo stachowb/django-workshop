4f8642137b450788d7281f1ff1f4ae57e1fcd096
stachowb <stachow.bartlomiej@gmail.com>
2021-04-18 00:25:31 +0200

    changed settings

--- booking/settings.py
+++ booking/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   BASE_DIR / "workshop/static"
+
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
