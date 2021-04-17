fc682907766ab33f98c53da5c4f9ec89861e2f07
stachowb <stachow.bartlomiej@gmail.com>
2021-04-18 00:34:54 +0200

    changed settings

--- booking/settings.py
+++ booking/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g@)uf-ub5bowl+v-+90qrimh12^yn0r0sfo@-_b4@@h&$3s5#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["djangobooking.herokuapp.com", "127.0.0.1"]
 
